6ec441e8f12a5cc74842ca89817e3de895f942b2
Patrick Porto <patrick.porto@loft.com.br>
2020-12-01 01:43:56 -0300

    Add windowing

--- src/Feed.js
+++ src/Feed.js
@@ -1,11 +1,14 @@
 import React from "react"
 import { Post } from "./Post"
 import { SortableContainer } from 'react-sortable-hoc';
+import LazyLoad from 'react-lazyload';
 
 export const Feed = SortableContainer(({ posts }) => {
     return <ul>
         {posts.map((post, index) => (
-            <Post key={index} index={index} post={post} />
+            <LazyLoad unmountIfInvisible>
+                <Post key={index} index={index} post={post} />
+            </LazyLoad>
         ))}
     </ul>
 })
